14dcc4033f445dc6db5a17a826e1eff780aaf91e
ValterSantos23 <76748981+ValterSantos23@users.noreply.github.com>
2022-07-17 13:06:35 -0300

    Adicionei arquivos via upload

---
+++ GitEclipse/src/br/com/valtersantos/git/HelloGit.java
@@ -0,0 +1,14 @@
+package br.com.valtersantos.git;
+/**
+ * 
+ * @author Valter
+ * Exemplo de integração do Eclipse com GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, GitHub!!");
+
+	}
+
+}
